76c8c5a4a8caa13ebe170e632716e63440fb1fc4
mikail <41960652+nerdropss@users.noreply.github.com>
2021-11-25 13:21:48 +0300

    new project

---
+++ lib/dd.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class dd extends StatelessWidget {
+  const dd({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
