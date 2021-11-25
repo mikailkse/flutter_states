f1a2b85138f3f639f35fcacb39abe4b607d6d938
mikail <41960652+nerdropss@users.noreply.github.com>
2021-11-25 13:26:58 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stateles_project/dd.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,16 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: dd(),
     );
   }
 }
